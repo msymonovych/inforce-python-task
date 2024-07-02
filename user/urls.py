b303ff0cc91a667c11d1f803ac937cd595cbe457
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-07-02 15:54:36 +0300

    Add user.urls to core.urls

---
+++ user/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from user.views import CreateUserView, ManageUserView
+
+urlpatterns = [
+    path("signup/", CreateUserView.as_view(), name="signup"),
+    path("profile/", ManageUserView.as_view(), name="profile"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+]
+
+app_name = "user"
