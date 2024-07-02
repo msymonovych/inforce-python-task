08b9a8bfdf886e4edc96980ff027230f9e900fa3
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-07-02 15:12:21 +0300

    Register models in admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from restaurant.models import (
+    Restaurant,
+    Menu,
+    MenuItem,
+    Vote,
+)
+
+
+admin.site.register(Restaurant)
+admin.site.register(Menu)
+admin.site.register(MenuItem)
+admin.site.register(Vote)
